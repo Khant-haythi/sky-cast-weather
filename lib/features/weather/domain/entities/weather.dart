47a4a5feda967c03f4eb8e24459085961d41bb7b
Khant-haythi <khanthaythi@gmail.com>
2026-01-30 07:05:25 +0630

    feat(domain):Update Weather model with hourly forecast data structures

--- lib/features/weather/domain/entities/weather.dart
+++ lib/features/weather/domain/entities/weather.dart
@@ -6,6 +6,7 @@ class Weather {
   final String condition;
   final String conditionIconId;
   final List<DailyForecast> dailyForecasts;
+  final List<DayDetailedForecast> detailedForecasts;
 
   Weather({
     required this.cityName,
@@ -14,8 +15,14 @@ class Weather {
     required this.condition,
     required this.conditionIconId,
     required this.dailyForecasts,
+    required this.detailedForecasts,
   });
+
+  double get currentTempF => (currentTemp * 9 / 5) + 32;
+
+
 }
+
 
 class DailyForecast {
   final DateTime date;
@@ -23,6 +30,7 @@ class DailyForecast {
   final double minTemp;
   final String condition;
   final String iconId;
+  final int weatherCode;
 
   DailyForecast({
     required this.date,
@@ -30,7 +38,23 @@ class DailyForecast {
     required this.minTemp,
     required this.condition,
     required this.iconId,
+    required this.weatherCode,
   });
 
   double get temp => maxTemp;
+}
+
+class DayDetailedForecast {
+  final DateTime date;
+  final List<HourlySlot> slots;
+
+  DayDetailedForecast({required this.date, required this.slots});
+}
+
+class HourlySlot {
+  final String time;
+  final double temp;
+  final int weatherCode;
+
+  HourlySlot({required this.time, required this.temp, required this.weatherCode});
 }
