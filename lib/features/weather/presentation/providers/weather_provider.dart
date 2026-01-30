5756031ddceea4344618592174e6cc69635dae6e
Khant-haythi <khanthaythi@gmail.com>
2026-01-30 13:01:15 +0630

    fix(provider): initialize CancelToken to prevent null check operator crash

--- lib/features/weather/presentation/providers/weather_provider.dart
+++ lib/features/weather/presentation/providers/weather_provider.dart
@@ -1,15 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:dio/dio.dart';
 import '../../data/repositories/weather_repository_impl.dart';
 import '../../domain/entities/weather.dart';
 
-
-// 1. Repository Provider
 final weatherRepositoryProvider = Provider((ref) => WeatherRepositoryImpl());
 
-// 2. Unit Provider (true = Celsius, false = Fahrenheit)
-final unitProvider = StateProvider<bool>((ref) => true);
-
-// 3. Weather State Notifier Provider
 final weatherProvider = StateNotifierProvider<WeatherNotifier, AsyncValue<Weather>>((ref) {
   final repo = ref.watch(weatherRepositoryProvider);
   return WeatherNotifier(repo);
@@ -17,15 +13,34 @@ final weatherProvider = StateNotifierProvider<WeatherNotifier, AsyncValue<Weathe
 
 class WeatherNotifier extends StateNotifier<AsyncValue<Weather>> {
   final WeatherRepositoryImpl _repository;
+  CancelToken? _cancelToken;
+
   WeatherNotifier(this._repository) : super(const AsyncValue.loading());
 
   Future<void> fetchWeather(String city) async {
+
+    _cancelToken?.cancel();
+
+    _cancelToken = CancelToken();
+
     state = const AsyncValue.loading();
+
     try {
-      final weather = await _repository.getWeather(city);
-      state = AsyncValue.data(weather); // Success: wraps data in AsyncValue
+
+      final weather = await _repository.getWeather(city, _cancelToken!);
+
+      if (mounted) {
+        state = AsyncValue.data(weather);
+      }
     } catch (e, stack) {
-      state = AsyncValue.error(e, stack); // Error: wraps error for the UI
+      if (e is DioException && CancelToken.isCancel(e)) {
+        debugPrint("Old request cancelled successfully");
+        return;
+      }
+
+      if (mounted) {
+        state = AsyncValue.error(e, stack);
+      }
     }
   }
 }
